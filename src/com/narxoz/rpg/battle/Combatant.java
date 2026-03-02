87b5b389014010cfd4b54584213d3f38dd5ed81f
Arbuz667 <emil.khalitov@narxoz.kz>
2026-03-02 16:41:15 +0500

    feat: add core combat interfaces and result model

--- src/com/narxoz/rpg/battle/Combatant.java
+++ src/com/narxoz/rpg/battle/Combatant.java
@@ -1,8 +1,12 @@
 package com.narxoz.rpg.battle;
 
 public interface Combatant {
+
     String getName();
+
     int getAttackPower();
+
     void takeDamage(int amount);
+
     boolean isAlive();
 }
